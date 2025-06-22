157947e963babd535d12713a62c4d350a71fe47a
Massatrio16 <dimassetosatrio@gmail.com>
2025-06-22 19:06:53 +0700

    tes

---
+++ twrp_X6528.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+DEVICE_PATH := device/infinix/X6528
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+
+# Inherit from RMX3261 device
+$(call inherit-product, device/infinix/X6528/device.mk)
+
+PRODUCT_DEVICE := X6528
+PRODUCT_NAME := twrp_X6528
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := X6528
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
