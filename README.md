59a5d31b7b883ae789b7b2d56352865bcba105d5
Massatrio16 <dimassetosatrio@gmail.com>
2025-07-09 20:51:40 +0700

    Clean Up

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Infinix Hot 40i (infinix-X6528B)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
--- twrp_X6528.mk
+++ twrp_X6528.mk
@@ -7,16 +7,25 @@
 DEVICE_PATH := device/infinix/X6528
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Inherit some common twrp stuff.
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
+# Inherit some common pb stuff.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
 
 # Inherit from X6528 device
 $(call inherit-product, device/infinix/X6528/device.mk)
